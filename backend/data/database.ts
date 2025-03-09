a3856bd92ed4825d131717aa78898dec96cc3bdf
Malin Jansson <maalin.jaansson@gmail.com>
2025-03-09 17:42:16 +0100

    added mobile styling

--- backend/data/database.ts
+++ backend/data/database.ts
@@ -3,9 +3,9 @@ import { Auction } from "./auction";
 export let auctions:Array<Auction> = []
 
 export function Init(){
-    auctions.push(new Auction("M-12", "Camera",{name:"", price:200}, "src/assets/camera.jpg", 200))
-    auctions.push(new Auction("A-67", "Sofa",{name:"", price:150}, "src/assets/sofa.jpg", 150))
-    auctions.push(new Auction("S-991","Pink plate",{name:"", price:50}, "src/assets/pinkplate.jpg", 50))
+    auctions.push(new Auction("M-12", "Camera",{name:"None", price:200}, "src/assets/camera.jpg", 200))
+    auctions.push(new Auction("A-67", "Sofa",{name:"None", price:150}, "src/assets/sofa.jpg", 150))
+    auctions.push(new Auction("S-991","Pink plate",{name:"None", price:50}, "src/assets/pinkplate.jpg", 50))
 };
 
 
